747ce14457c6eefd97b4ca5932130d40939510e4
賀好昭仁 <kako@qnote.co.jp>
2016-12-13 02:34:48 +0900

    [add]Mandelbrot Terrain

---
+++ Assets/MandelbrotCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MandelbrotCalculator
+{
+    double exponent = 2.0;
+    int maxIteration = 50;
+    double threshold = 2.0;
+
+    public MandelbrotCalculator(double exponent, int maxIteration, double threshold)
+    {
+        this.exponent = exponent;
+        this.maxIteration = maxIteration;
+        this.threshold = threshold;
+    }
+
+    public float Calculate(double baseX, double baseY)
+    {
+        Vector2 z = Vector2.zero;
+        var x = 0.0;
+        var y = 0.0;
+        for (int i = 0; i < maxIteration; i++)
+        {
+            var a = x == 0 ? 0.0 : Math.Atan2(y, x) * exponent;
+            var powed = Math.Pow(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)), exponent);
+            x = Math.Cos(a) * powed;
+            y = Math.Sin(a) * powed;
+            x += baseX;
+            y += baseY;
+            if (Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) > threshold)
+            {
+                return (float)i / maxIteration;
+            }
+        }
+        return 1f;
+    }
+}
---
+++ Assets/MandelbrotTerrain.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MandelbrotTerrain : MonoBehaviour
+{
+    double baseX = -1.75;
+    double baseY = 0.0;
+    double size = 10.0;
+    MandelbrotCalculator calculator;
+
+    void Start()
+    {
+        calculator = new MandelbrotCalculator(2.0, 20, 2.0);
+        StartCoroutine(UpdateTerrainCoroutine());
+    }
+
+    IEnumerator UpdateTerrainCoroutine()
+    {
+        while (true)
+        {
+            size *= 0.98;
+
+            var terrain = GetComponent<Terrain>();
+            var terrainData = terrain.terrainData;
+
+            var heights = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+
+            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            {
+                for (int x = 0; x < terrainData.heightmapWidth; x++)
+                {
+                    var coordinateX = baseX - size / 2.0 + (double)x / terrainData.heightmapWidth * size;
+                    var coordinateY = baseY - size / 2.0 + (double)y / terrainData.heightmapHeight * size;
+                    heights[x, y] = calculator.Calculate(coordinateX, coordinateY);
+                }
+            }
+            terrainData.SetHeights(0, 0, heights);
+            yield return null;
+        }
+    }
+}
