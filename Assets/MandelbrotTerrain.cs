0eb6ea396f5eb8910eeb8971cd05dc821431d0cc
賀好昭仁 <kako@qnote.co.jp>
2016-12-13 12:07:04 +0900

    [clean]remove unused data

--- Assets/MandelbrotTerrain.cs
+++ Assets/MandelbrotTerrain.cs
@@ -3,38 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(Terrain))]
 public class MandelbrotTerrain : MonoBehaviour
 {
+    [SerializeField]
     double baseX = -1.75;
+    [SerializeField]
     double baseY = 0.0;
-    double size = 10.0;
+    [SerializeField]
+    double renderArea = 10.0;
+
+    TerrainData terrainData;
 
     void Start()
     {
+        terrainData = GetComponent<Terrain>().terrainData;
         StartCoroutine(UpdateTerrainCoroutine());
     }
 
     IEnumerator UpdateTerrainCoroutine()
     {
+        var heights = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+
         while (true)
         {
-            size *= 0.98;
+            renderArea *= 0.98;
+            var startX = baseX - renderArea / 2.0;
+            var startY = baseY - renderArea / 2.0;
+            var renderAreaPerWidth = renderArea / terrainData.heightmapWidth;
+            var renderAreaPerHeight = renderArea / terrainData.heightmapHeight;
 
-            var terrain = GetComponent<Terrain>();
-            var terrainData = terrain.terrainData;
-
-            var heights = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
-
-            for (int y = 0; y < terrainData.heightmapHeight; y++)
+            for (int x = 0; x < terrainData.heightmapWidth; x++)
             {
-                for (int x = 0; x < terrainData.heightmapWidth; x++)
+                for (int y = 0; y < terrainData.heightmapHeight; y++)
                 {
-                    var coordinateX = baseX - size / 2.0 + (double)x / terrainData.heightmapWidth * size;
-                    var coordinateY = baseY - size / 2.0 + (double)y / terrainData.heightmapHeight * size;
-                    heights[x, y] = MandelbrotCalculator.Calculate(coordinateX, coordinateY);
+                    heights[x, y] = MandelbrotCalculator.Calculate(
+                        startX + x * renderAreaPerWidth,
+                        startY + y * renderAreaPerHeight);
                 }
             }
+
             terrainData.SetHeights(0, 0, heights);
+
             yield return null;
         }
     }
